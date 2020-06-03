291ae5e7b34ca65f824bf006ccd91cb6463f75cb
dominik-billensteiner <56152933+dominik-billensteiner@users.noreply.github.com>
2020-06-03 14:59:02 +0200

    Composed markup for Tour component and added basic styles

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -4,14 +4,19 @@ import "./Tour.scss";
 const Tour = () => {
   return (
     <div className="tour">
-      <h2 className="tour__date">31.05.2020</h2>
-      <h2 className="tour__mountain">Schoberstein</h2>
-      <p className="tour__elevation">1285</p>
-      <div className="elevation-gain__wrapper">
-        <p>965</p>
+      <div className="tour__container">
+        <div className="tour__mountainer-container">
+          <h2 className="tour__mountain">Schoberstein</h2>
+          <h3 className="tour__date">31.05.2020</h3>
+        </div>
+        <h3 className="tour__elevation">1285</h3>
       </div>
-      <div className="distance__wrapper"></div>
-      <p>10.5</p>
+      <div className="tour__container">
+        <h3 className="tour__distance">12.5</h3>
+        <h3 className="tour__duration">3:10</h3>
+        <h3 className="tour__ascent">965</h3>
+        <h3 className="tour__descent">980</h3>
+      </div>
     </div>
   );
 };
