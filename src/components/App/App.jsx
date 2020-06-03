50545ec92e55836a275c7b1ce22d396a23c6b154
dominik-billensteiner <56152933+dominik-billensteiner@users.noreply.github.com>
2020-06-03 15:44:54 +0200

    Added props to Tour component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,16 @@ import Tour from "../Tour/Tour";
 function App() {
   return (
     <div className="App">
-      <Tour />
+      <Tour
+        mountain="Schoberstein"
+        elevation="1285"
+        date="29.04.2020"
+        description="Südanstieg Molln"
+        distance="9.1"
+        duration="3:15"
+        ascent="880"
+        descent="880"
+      />
     </div>
   );
 }
--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -2,21 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Tour.scss";
 
-const Tour = ({ mountain, date, distance, duration, ascent, descent }) => {
+const Tour = ({
+  mountain,
+  elevation,
+  description,
+  date,
+  distance,
+  duration,
+  ascent,
+  descent,
+}) => {
   return (
     <div className="tour">
       <div className="tour__container">
         <div className="tour__mountainer-container">
-          <h2 className="tour__mountain">Schoberstein</h2>
-          <h3 className="tour__date">31.05.2020</h3>
+          <h2 className="tour__mountain">
+            {mountain}, {elevation} m
+          </h2>
+          <h3 className="tour__date">{date}</h3>
         </div>
-        <h3 className="tour__elevation">1285</h3>
+        <h3 className="tour__description">{description}</h3>
       </div>
       <div className="tour__container">
-        <h3 className="tour__distance">12.5</h3>
-        <h3 className="tour__duration">3:10</h3>
-        <h3 className="tour__ascent">965</h3>
-        <h3 className="tour__descent">980</h3>
+        <h3 className="tour__distance">{distance} km</h3>
+        <h3 className="tour__duration">{duration} h</h3>
+        <h3 className="tour__ascent">{ascent} hm</h3>
+        <h3 className="tour__descent">{descent} hm</h3>
       </div>
     </div>
   );
