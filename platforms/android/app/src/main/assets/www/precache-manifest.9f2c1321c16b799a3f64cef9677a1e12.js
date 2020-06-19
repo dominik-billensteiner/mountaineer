e6d06597772c0ed86be1f73ee480f1d90fbaa7ea
dominik-billensteiner <56152933+dominik-billensteiner@users.noreply.github.com>
2020-06-19 13:32:06 +0200

    Implemented android OS support of app  with Cordova

---
+++ platforms/android/app/src/main/assets/www/precache-manifest.9f2c1321c16b799a3f64cef9677a1e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e73bc2abebd0901bce585e7907e874a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81594acf535fa5868614",
+    "url": "./static/css/main.ceb71927.chunk.css"
+  },
+  {
+    "revision": "a784b673921070bdb2e0",
+    "url": "./static/js/2.aeb8e6cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.aeb8e6cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81594acf535fa5868614",
+    "url": "./static/js/main.beb20ae5.chunk.js"
+  },
+  {
+    "revision": "e4cc0a894c88c7edb836",
+    "url": "./static/js/runtime-main.25d21227.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import "./index.css";
 import Mountaineer from "./components/Mountaineer/Mountaineer";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Mountaineer />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const startApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Mountaineer />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.register();
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (window.cordova) {
+  // for iOS / Android
+  document.addEventListener("deviceready", startApp, false);
+} else {
+  // for Web
+  startApp();
+}
